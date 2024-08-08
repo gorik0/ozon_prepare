37efbe3df69bfd84f7524117671cd0d1ad1b4b9c
gorik0 <delavarus@gmail.com>
2024-08-08 09:29:07 +0300

    6_work_half

--- ozone_test_round/6/main.go
+++ ozone_test_round/6/main.go
@@ -2,55 +2,52 @@ package main
 
 import (
 	"bufio"
-	"encoding/json"
 	"fmt"
-	"log"
 	"math"
 	"os"
 	"sort"
-	"strings"
+	"time"
 )
 
 func main() {
 	var (
 		dataCount, boxesCount, carsCount, carTonnage int
-		boxes                                        []float64
+		boxes                                        []int
+		logistics                                    []int
 		in                                           *bufio.Reader
 		out                                          *bufio.Writer
 	)
 
+	ti := time.Now()
 	_ = boxesCount
 	_ = boxes
 	in = bufio.NewReader(os.Stdin)
+	logistics = make([]int, 0)
 	out = bufio.NewWriter(os.Stdout)
 
 	defer out.Flush()
 
-	fmt.Fscanln(in, &dataCount)
+	fmt.Fscan(in, &dataCount)
 	for range dataCount {
-		fmt.Fscanln(in, &carsCount, &carTonnage)
+		fmt.Fscan(in, &carsCount, &carTonnage)
 
-		s, _ := in.ReadString('\n')
-		println(s)
-		s = strings.Replace(s, " ", ",", -1)
-		s = "[" + s + "]"
-		er := json.Unmarshal([]byte(s), &boxes)
-		log.Println(er)
-
-		for i, el := range boxes {
-			boxes[i] = math.Pow(float64(2), el)
+		fmt.Fscan(in, &boxesCount)
+		boxes = make([]int, boxesCount)
+		for i := range boxesCount {
+			fmt.Fscan(in, &boxes[i])
 
 		}
-		log.Println("Tonnage ::::", carTonnage)
-		log.Println("Al ::::: ", boxes)
 
-		//remain := EquipCar(boxes, float64(carTonnage))
+		for i, el := range boxes {
+			boxes[i] = int(math.Pow(2, float64(el)))
+
+		}
+		//remain := EquipCar(boxes, int(carTonnage))
 		//log.Println("REMAIN :::: ", remain)
 		logistic := 0
 		carsRemain := carsCount
 		for {
-			boxes = EquipCar(boxes, float64(carTonnage))
-			log.Printf("Boxes remain ::: %v\n", boxes)
+			boxes = EquipCar(boxes, int(carTonnage))
 			carsRemain--
 			if len(boxes) != 0 && carsRemain == 0 {
 				carsRemain = carsCount
@@ -60,16 +57,22 @@ func main() {
 				break
 			}
 		}
-		log.Printf("Logisstic ::: %v\n", logistic)
+		logistics = append(logistics, logistic)
 		//Run(in, out, boxes, carsCount, carTonnage)
 	}
 
+	for _, i := range logistics {
+
+		fmt.Fprintln(out, i)
+	}
+	println(time.Since(ti).Seconds())
+
 }
 
-func EquipCar(boxes []float64, tonnage float64) (boxesRemain []float64) {
+func EquipCar(boxes []int, tonnage int) (boxesRemain []int) {
 	var offset int
 	sort.Slice(boxes, func(i, j int) bool { return boxes[i] > boxes[j] })
-	boxesRemain = make([]float64, len(boxes))
+	boxesRemain = make([]int, len(boxes))
 	copy(boxesRemain, boxes)
 	for i, box := range boxes {
 		if box > tonnage {
@@ -106,47 +109,20 @@ func EquipCar(boxes []float64, tonnage float64) (boxesRemain []float64) {
 	return
 }
 
-func Run(in *bufio.Reader, out *bufio.Writer, boxes []float64, carCount int, tonnage int) {
-
-	//	::: Sort boxes
-	log.Println(boxes)
-	sort.Slice(boxes, func(i, j int) bool { return boxes[i] > boxes[j] })
-	log.Println(boxes)
-	boxesRemain := FindOptimusForCar(boxes, float64(tonnage))
-	log.Println("boxesRemain:::", boxesRemain)
-}
-
-func FindOptimusForCar(boxes []float64, tonnage float64) (boxesrRemain []float64) {
-	log.Println("BOXES", boxes)
-	copy(boxesrRemain, boxes)
-	sort.Slice(boxes, func(i, j int) bool { return boxes[i] > boxes[j] })
-	for i, box := range boxes {
-		if box == tonnage {
-			if i == 0 {
-				return boxesrRemain[1:]
-			}
-			return append(boxesrRemain[:i], boxesrRemain[i+1:]...)
-		} else if tonnage > box {
-			tonnage -= box
-			if i == 0 {
-				boxesrRemain = FindOptimusForCar(append(boxesrRemain[1:]), tonnage)
-			} else {
-
-				boxesrRemain = FindOptimusForCar(append(boxesrRemain[:i], boxesrRemain[i+1:]...), tonnage)
-			}
-
-		}
-
-	}
-	return boxesrRemain
-
-}
-
 /*
 
-1
-2 10
-1 0 2 1 1 3 1
+	1
+	2 10
+	3 1 3 1 3 1 3 1 0
+
+	1
+	2 10
+	1
+
+	1
+	2 17
+	6
+	0 3 1 4 3 3
 
 
 
